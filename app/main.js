46ba603562120fc283425f483bb93909b5228388
Akshay Gupta <akshayg1047.ag@gmail.com>
2024-10-23 01:20:06 +0530

    PING/PONG

--- app/main.js
+++ app/main.js
@@ -3,7 +3,12 @@ const net = require("net");
 
 
 const server = net.createServer((connection) => {
-  // Handle connection
+    console.log("coonection etablished");
+    connection.write('+PONG\r\n');
 });
 
 server.listen(6379, "127.0.0.1");
+
+server.on("error", (err) => {
+    console.error("Server error:", err);
+});
