f6e01bdf15d9f5404854387b1334c59bd26f8a91
Akshay Gupta <akshayg1047.ag@gmail.com>
2024-10-23 01:46:08 +0530

    some improvisation

--- app/main.js
+++ app/main.js
@@ -4,13 +4,23 @@ const net = require("net");
 
 const server = net.createServer((connection) => {
     connection.on("data", (data) => {
-    console.log(data)
-    connection.write('\+PONG\r\n');
+    const dataStr = data.toString().toLowerCase()
+    if (dataStr === '*1\r\n$4\r\nping\r\n') {
+    connection.write('\+PONG\r\n');}
     })
+    connection.on("close", () => {
+        console.log("Connection closed");
+        connection.end();
+      })
 });
+
+process.on('exit', () => {
+    console.log("Server closed gracefully.");
+    server.close();
+  })
 
 server.listen(6379, "127.0.0.1");
 
 server.on("error", (err) => {
-    console.error("Server error:", err);
+    console.log("Server error:", err);
 });
