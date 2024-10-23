e88824033701cc3aebb5bf969d191c7a59b11565
Akshay Gupta <akshayg1047.ag@gmail.com>
2024-10-23 19:12:59 +0530

    added ECHO cmd support

--- app/main.js
+++ app/main.js
@@ -4,9 +4,14 @@ const net = require("net");
 
 const server = net.createServer((connection) => {
     connection.on("data", (data) => {
-    const dataStr = data.toString().toLowerCase()
-    if (dataStr === '*1\r\n$4\r\nping\r\n') {
+    const dataStr = data.toString()
+    if (dataStr.toLowerCase() === '*1\r\n$4\r\nping\r\n') {
     connection.write('\+PONG\r\n');}
+
+    else if (dataStr.split('\r\n')[2].toLowerCase() === "echo") {
+        var echoStr = dataStr.split('\r\n')[3] + "\r\n" + dataStr.split('\r\n')[4] + "\r\n"
+        connection.write(echoStr);  
+    }
     })
     connection.on("close", () => {
         console.log("Connection closed");
